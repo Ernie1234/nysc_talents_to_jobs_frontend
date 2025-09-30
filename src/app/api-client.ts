8841e33dd62e271fc1fca1b50799b98f9d052c63
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-09-30 12:48:02 +0100

    feat: revert backend url

--- src/app/api-client.ts
+++ src/app/api-client.ts
@@ -2,9 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "./store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl:
-    import.meta.env.VITE_PUBLIC_API_URL || "http://localhost:5000/api/v1",
-  // dont forget to remove the public from the url in production
+  baseUrl: import.meta.env.VITE_API_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const auth = (getState() as RootState).auth;
